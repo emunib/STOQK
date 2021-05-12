53c2d80f99217752a8dcb3788c867f2dc2ecfb9b
Ehtasham Munib <emunib@outlook.com>
2021-05-11 20:17:44 -0600

    Set up Express app

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Split Translation of the Holy Quran'});
+});
+
+module.exports = router;
