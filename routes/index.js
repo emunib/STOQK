f3e3b51d2d7be1c93f31a75c9ba9ec2b6de43545
Ehtasham Munib <emunib@outlook.com>
2021-06-11 08:57:30 -0600

    Add slide partial

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Split Translation of the Holy Quran'});
+    res.render('index', {title: 'Split Translation of the Holy Quran', img_paths: ['https://picsum.photos/900/300','https://picsum.photos/200/900','https://picsum.photos/200/300']});
 });
 
 module.exports = router;
