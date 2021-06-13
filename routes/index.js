4fd8c030d4a5da81d1825d7de8a1cab143155f03
Ehtasham Munib <emunib@outlook.com>
2021-06-12 23:55:08 -0600

    Fix slide partial, add test img paths

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Split Translation of the Holy Quran', img_paths: ['https://picsum.photos/900/300','https://picsum.photos/200/900','https://picsum.photos/200/300']});
+    res.render('index', {
+        title: 'Split Translation of the Holy Quran',
+        img_paths: [
+            'https://via.placeholder.com/500x1600?text=1',
+            'https://via.placeholder.com/2000x500/808080?text=2',
+            'https://via.placeholder.com/5000x1800?text=3',
+            'https://via.placeholder.com/1000x800/808080?text=4',
+            'https://via.placeholder.com/1500x800?text=5',
+            'https://via.placeholder.com/200x500/808080?text=6',
+            'https://via.placeholder.com/4000x600?text=7',
+            'https://via.placeholder.com/8000x700/808080?text=8',
+            'https://via.placeholder.com/500x1600?text=9',
+            'https://via.placeholder.com/3000x800/808080?text=10'
+        ]
+    });
 });
 
 module.exports = router;
